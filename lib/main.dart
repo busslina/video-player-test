7e2193e5731162c4baeaa44cb9c31692fdae307c
Vicente <busslina@gmail.com>
2024-05-20 22:22:37 +0200

    snapshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rearch/flutter_rearch.dart';
+import 'package:rearch/rearch.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RearchBootstrapper(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -11,10 +15,46 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: _Home(),
       ),
     );
+  }
+}
+
+class _Home extends RearchConsumer {
+  const _Home();
+
+  static const _sampleVideoUrl = 'https://busslina.com/public/sample-30s.mp4';
+
+  @override
+  Widget build(BuildContext context, WidgetHandle use) {
+    final initialized = use.data(false);
+
+    final videoController = use.disposable(
+        () => CachedVideoPlayerController.network(_sampleVideoUrl),
+        (controller) => controller.dispose());
+
+    final customVideoController =
+        use.callonce(() => CustomVideoPlayerWebController(
+                webVideoPlayerSettings: const CustomVideoPlayerWebSettings(
+              src: _sampleVideoUrl,
+            )));
+
+    final initCancelableOperation = use.callonce(() =>
+        CancelableOperation.fromFuture(videoController.initialize())
+          ..value.then((_) => initialized.value = true));
+
+    // Cancelling init operation on dispose
+    use.effect(
+      () {
+        return () {
+          initCancelableOperation.cancel();
+        };
+      },
+      [],
+    );
+
+    return CustomVideoPlayerWeb(
+        customVideoPlayerWebController: customVideoController);
   }
 }
